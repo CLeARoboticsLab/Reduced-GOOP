8d67f12bde22cb886b48631243da725d10217068
dfridovi <dfk@utexas.edu>
2025-07-18 09:50:28 -0500

    KKT system struct is done

--- src/primal_dual.jl
+++ src/primal_dual.jl
@@ -28,48 +28,30 @@ function GOOPKKTSystem(
     F::Vector{Symbolics.Num},
     z_symbolic::Vector{Symbolics.Num},
     θ_symbolic::Vector{Symbolics.Num},
+    ϵ_symbolic::Symbolics.Num,
     preference_slack_dims,
     interior_point_slack_dims,
     inequality_constraint_dual_dims;
     backend_options = (;),
 )
-    # F!(val, x, y, s; θ) computes the primal-dual system equation in-place.
-    # ∇F!(val, x, y, s; θ) computes the Jacobian of the primal-dual system equation in-place.
-
-    backend = SymbolicTracingUtils.SymbolicsBackend()
-
-    s_symbolic = SymbolicTracingUtils.make_variables(backend, :s, length(y_symbolic))
-    ϵ_symbolic = only(SymbolicTracingUtils.make_variables(backend, :ϵ, 1))
-    z_symbolic = [x_symbolic; y_symbolic; s_symbolic]
-
-    F_symbolic = [
-        G_symbolic
-        H_symbolic - s_symbolic
-        s_symbolic .* y_symbolic .- ϵ_symbolic
-    ]
-
     F! = let
         _F! = SymbolicTracingUtils.build_function(
             F_symbolic,
-            x_symbolic,
-            y_symbolic,
-            s_symbolic,
+            z_symbolic,
             θ_symbolic,
             ϵ_symbolic;
             in_place = true,
             backend_options,
         )
 
-        (result, x, y, s; θ, ϵ) -> _F!(result, x, y, s, θ, ϵ)
+        (result, z; θ, ϵ) -> _F!(result, z, θ, ϵ)
     end
 
     ∇F_z! = let
         ∇F_symbolic = SymbolicTracingUtils.sparse_jacobian(F_symbolic, z_symbolic)
         _∇F! = SymbolicTracingUtils.build_function(
             ∇F_symbolic,
-            x_symbolic,
-            y_symbolic,
-            s_symbolic,
+            z_symbolic,
             θ_symbolic,
             ϵ_symbolic;
             in_place = true,
@@ -80,7 +62,7 @@ function GOOPKKTSystem(
         constant_entries =
             SymbolicTracingUtils.get_constant_entries(∇F_symbolic, z_symbolic)
         SymbolicTracingUtils.SparseFunction(
-            (result, x, y, s; θ, ϵ) -> _∇F!(result, x, y, s, θ, ϵ),
+            (result, z; θ, ϵ) -> _∇F!(result, z, θ, ϵ),
             rows,
             cols,
             size(∇F_symbolic),
@@ -88,5 +70,11 @@ function GOOPKKTSystem(
         )
     end
 
-    PrimalDualSys(F!, ∇F_z!, length(x_symbolic), length(y_symbolic), dims) # return struct
+    GOOPKKTSystem(
+        F!,
+        ∇F_z!,
+        preference_slack_dims,
+        interior_point_slack_dims,
+        inequality_constraint_dual_dims;
+    )
 end
