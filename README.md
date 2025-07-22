b252fb4adc2571fcbc751410a39f9090a975bba9
dfridovi <dfk@utexas.edu>
2025-07-22 15:28:47 -0500

    adding CI

---
+++ README.md
@@ -0,0 +1,4 @@
+# QuasiGOOP.jl
+
+[![CI](https://github.com/CLeARoboticsLab/QuasiGOOP.jl/actions/workflows/test.yml/badge.svg)](https://github.com/CLeARoboticsLab/QuasiGOOP.jl/actions/workflows/test.yml)
+[![License](https://img.shields.io/badge/license-BSD-new)](https://opensource.org/license/bsd-3-clause)
---
+++ test/runtests.jl
@@ -0,0 +1,16 @@
+using Test: @testset, @test
+
+using QuasiGOOP
+using BlockArrays: BlockArray, Block, mortar, blocks
+
+@testset "TODO: @DongHo put in a real test" begin
+    """ TODO
+    Test for the following QP:
+                               min_x 0.5 xᵀ M x - θᵀ x
+                               s.t.  Ax - b ≥ 0.
+    Taking `y ≥ 0` as a Lagrange multiplier, we obtain the KKT conditions:
+                                 G(x, y) = Mx - Aᵀy - θ = 0
+                                 0 ≤ y ⟂ H(x, y) = Ax - b ≥ 0.
+    """
+    @test true
+end
