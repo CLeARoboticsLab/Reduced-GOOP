7b9db5672848db4694c9caba4eb597247b70d184
Dong Ho Lee <dhl@Dongs-Mac-mini.local>
2026-02-26 08:14:45 -0600

    run_4 reduced system with alpha (contraction) 0.5

--- experiments/Intersection.jl
+++ experiments/Intersection.jl
@@ -258,8 +258,8 @@ function demo(; map_end = 7, lane_width = 2, verbose = false, rng_seed = 123)
 	planning_horizon = 6
 	collision_avoidance = 1.5
 	num_instances = 10
-	epsilon_schedule = [1.0, 0.5, 0.1]
-	max_inner_iters_schedule = [50, 100, 100]
+	epsilon_schedule = [1.0]
+	max_inner_iters_schedule = [50]
 	perturbation_scale = 0.2
 	linesearch = :backtracking # :backtracking, :fraction_to_boundary
 	goop_version = :reduced # :complete, :reduced 
